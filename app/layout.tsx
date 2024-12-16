e7fc65d94b18cd04d394943f407078290b09619e
prajjwal2-3 <121849327+prajjwal2-3@users.noreply.github.com>
2024-12-16 18:18:56 +0530

    fix auth page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,15 @@
 import type { Metadata } from "next";
-import { DM_Sans } from 'next/font/google'
+import { DM_Sans } from "next/font/google";
 import "./globals.css";
-import "./prosemirror.css"
+import "./prosemirror.css";
 import "@uploadthing/react/styles.css";
-import {getKindeServerSession} from "@kinde-oss/kinde-auth-nextjs/server";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import Header from "@/components/wrapper/Header";
 import Sidebar from "@/components/sidebar/Sidebar";
 import LandingPage from "@/components/LandingPage";
-import { Toaster } from "@/components/ui/toaster"
-
-const inter = DM_Sans({ subsets:['latin']})
-
+import { Toaster } from "@/components/ui/toaster";
+import { redirect } from "next/navigation";
+const inter = DM_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Conqrr",
@@ -21,36 +20,39 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const {isAuthenticated,getUser}=getKindeServerSession()
-  const flag  = await isAuthenticated()
-  const user = await getUser()
-
+  const { isAuthenticated, getUser } = getKindeServerSession();
+  const flag = await isAuthenticated();
+  const user = await getUser();
+  if(!(await isAuthenticated())){
+        redirect(
+          '/api/auth/login'
+        )
+      }
   return (
-    <html lang="en" >
+    <html lang="en">
       <body className={inter.className}>
-     
-      <div className="max-w-screen-2xl shadow-md w-full mx-auto ">
-        <Header/>
-        {
-          flag&&user?.email?
-          // aur bhi aaega idhar
-          <div className=" flex pt-10 flex-row border-x">
-          <Sidebar/>
-       <div className="ml-[calc(100vw/6)] min-h-screen w-full">  {children}</div>
-         </div>:
-         <>
-         <Header/>
-         <div className="pt-16">
-          <LandingPage/>
-         
-         </div>
-         </>
-        }
-    
-    <Toaster />
-      </div>
-     
-        </body>
+        <div className="max-w-screen-2xl shadow-md w-full mx-auto ">
+          <Header />
+          {flag && user?.email ? (
+            <div className=" flex pt-10 flex-row border-x">
+              <Sidebar />
+              <div className="ml-[calc(100vw/6)] min-h-screen w-full">
+                {" "}
+                {children}
+              </div>
+            </div>
+          ) : (
+            <>
+              <Header />
+              <div className="pt-16">
+                <LandingPage />
+              </div>
+            </>
+          )}
+
+          <Toaster />
+        </div>
+      </body>
     </html>
   );
 }
